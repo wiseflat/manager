22fc6d9fa129c1cf47eac8e3c9c1f888d4968090
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-03-23 15:07:02 +0530

    refactor(cda): moved cda code from cloud apps to standalone module
    Signed-off-by: Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>

--- packages/manager/modules/cda/src/index.js
+++ packages/manager/modules/cda/src/index.js
@@ -1,18 +1,65 @@
 import angular from 'angular';
-
-import '@ovh-ux/manager-core';
 import '@uirouter/angularjs';
 import 'angular-translate';
+import 'ovh-api-services';
+import 'ovh-ui-angular';
+import 'angular-ui-bootstrap';
+import '@ovh-ux/ng-ovh-sidebar-menu';
 
-import component from './component';
+import ovhManagerCore from '@ovh-ux/manager-core';
+import ngAtInternet from '@ovh-ux/ng-at-internet';
+import ngOvhCloudUniverseComponents from '@ovh-ux/ng-ovh-cloud-universe-components';
+import ngOvhDocUrl from '@ovh-ux/ng-ovh-doc-url';
+
+import cdaDetails from './details';
+import cdaDetailsHome from './details/home';
+import cdaIp from './ip';
+import cdaPool from './pool';
+import cdaUser from './user';
 import routing from './routing';
+import service from './cda.service';
+
+import 'ovh-ui-kit-bs/dist/ovh-ui-kit-bs.css';
+import 'ovh-ui-kit/dist/oui.css';
+import './cda.less';
 
 const moduleName = 'ovhManagerCda';
 
 angular
-  .module(moduleName, ['ovhManagerCore', 'pascalprecht.translate', 'ui.router'])
+  .module(moduleName, [
+    'pascalprecht.translate',
+    'ui.router',
+    'ovh-api-services',
+    'oui',
+    'ui.bootstrap',
+    'ngOvhSidebarMenu',
+    ovhManagerCore,
+    ngAtInternet,
+    ngOvhDocUrl,
+    ngOvhCloudUniverseComponents,
+    cdaDetails,
+    cdaDetailsHome,
+    cdaIp,
+    cdaUser,
+    cdaPool,
+  ])
   .config(routing)
-  .component('cda', component)
+  .config(
+    /* @ngInject */ (
+      $qProvider,
+      ovhDocUrlProvider,
+      TranslateServiceProvider,
+    ) => {
+      ovhDocUrlProvider.setUserLocale(TranslateServiceProvider.getUserLocale());
+      $qProvider.errorOnUnhandledRejections(false);
+    },
+  )
+  .service('CdaService', service)
+  .run(($transitions, CdaService) => {
+    $transitions.onSuccess({ to: 'cda.**' }, (transition) => {
+      CdaService.initDetails(transition.params().serviceName);
+    });
+  })
   .run(/* @ngTranslationsInject:json ./translations */);
 
 export default moduleName;
--- packages/manager/modules/cda/src/routing.js
+++ packages/manager/modules/cda/src/routing.js
@@ -1,6 +1,11 @@
 export default /* @ngInject */ ($stateProvider) => {
-  $stateProvider.state('app', {
-    url: '/cda',
-    component: 'cda',
+  $stateProvider.state('cda', {
+    url: '/paas/cda',
+    template: '<div data-ui-view="cdaDetails"></div>',
+    translations: {
+      format: 'json',
+      value: ['.'],
+    },
+    abstract: true,
   });
 };
