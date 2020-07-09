1cf92f00382b4ee2d91489de7c1ce7e78349e1ca
Cyril Biencourt <cyril.biencourt@corp.ovh.com>
2020-07-09 12:12:00 +0200

    feat: poc analytics injection
    Signed-off-by: Cyril Biencourt <cyril.biencourt@corp.ovh.com>

--- packages/manager/apps/public-cloud/src/index.js
+++ packages/manager/apps/public-cloud/src/index.js
@@ -2,10 +2,13 @@ import 'script-loader!jquery'; // eslint-disable-line
 import 'whatwg-fetch';
 import { attach as attachPreloader } from '@ovh-ux/manager-preloader';
 import { bootstrapApplication } from '@ovh-ux/manager-core';
+import iframe from './analytics.html';
 
 attachPreloader();
 
 bootstrapApplication().then(({ region }) => {
+  document.getElementById('iframe').srcdoc = iframe;
+
   import(`./config-${region}`)
     .catch(() => {})
     .then(() => import('./app.module'))
